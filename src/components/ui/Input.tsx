210c0eb904f11721454d891b82b67eb27c9531c0
PaulHaze <dev@artemist.com.au>
2024-12-11 10:12:40 +1100

    style fixes to input ring

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
           'flex h-12 w-full rounded-md border border-input/10 bg-transparent px-3 py-1 text-base shadow-sm transition-colors',
           'placeholder:text-content-light/40',
           'file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground',
-          'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+          'focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
           'md:text-sm',
           className,
         )}
