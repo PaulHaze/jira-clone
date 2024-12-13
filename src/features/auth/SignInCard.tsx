607969e12038ad52a57d0bcdd1c125e740a88d39
PaulHaze <dev@artemist.com.au>
2024-12-14 07:18:13 +1100

    tidy up zod password validator

--- src/features/auth/SignInCard.tsx
+++ src/features/auth/SignInCard.tsx
@@ -23,15 +23,7 @@ import { GoogleLogo, GitHubLogo } from '@/components/ui/logos';
 
 const formSchema = z.object({
   email: z.string().email(),
-  password: z
-    .string()
-    .min(8, 'Password must be at least 8 characters long.')
-    .max(128, 'Password cannot exceed 128 characters.')
-    .regex(/[0-9]/, 'Password must contain at least one number.')
-    .regex(
-      /[!@#$%^&*(),.?":{}|<>]/,
-      'Password must contain at least one special character.',
-    ),
+  password: z.string(),
 });
 
 export function SignInCard() {
@@ -41,7 +33,6 @@ export function SignInCard() {
       email: '',
       password: '',
     },
-    // mode: 'onChange',
   });
 
   const onSubmit = (data: z.infer<typeof formSchema>) => {
