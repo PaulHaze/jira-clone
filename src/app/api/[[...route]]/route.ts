b9aceb1a43420e6860ba2fc473ae656c87200525
PaulHaze <dev@artemist.com.au>
2024-12-21 08:46:48 +1100

    hono cleanup

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -9,5 +9,15 @@ app.get('/hello', (c) => {
   return c.json({ message: 'Hello From Next.js' });
 });
 
+app.get('/hello/:name', (c) => {
+  const name = c.req.param('name');
+  return c.json({ message: `Hello ${name}` });
+});
+
+app.get('/project/:projectId', (c) => {
+  const { projectId } = c.req.param();
+  return c.json({ projectId });
+});
+
 export const GET = handle(app);
 export const POST = handle(app);
